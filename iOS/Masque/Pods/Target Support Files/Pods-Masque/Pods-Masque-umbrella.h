a79f95c823a51013f5d5ba599618c41ae139c9f6
Jason <jasonzhe@usc.edu>
2017-06-07 11:01:58 -0400

    fix git pushes

--- iOS/Masque/Pods/Target Support Files/Pods-Masque/Pods-Masque-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_MasqueVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_MasqueVersionString[];
-
--- iOS/Masque/Pods/Target Support Files/Pods-MasqueUITests/Pods-MasqueUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MasqueUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MasqueUITests
-@end
