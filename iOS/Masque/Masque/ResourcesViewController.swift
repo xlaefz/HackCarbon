33b9b91de8a20c465c1d31b5d3f9b864bd09b5ed
Jason <jasonzhe@usc.edu>
2017-06-07 15:25:18 -0400

    took out chat button

---
+++ iOS/Masque/Masque/ResourcesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ResourcesViewController.swift
+//  Masque
+//
+//  Created by Jason Zheng on 6/7/17.
+//  Copyright © 2017 Jason Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ResourcesViewController: NSObject {
+
+}
