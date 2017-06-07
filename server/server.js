91398fde0fd67910aaa505a0efdcd9674cd94f79
Kaveet Laxmidas <kaveetlaxmidas@gmail.com>
2017-06-07 14:14:44 -0400

    Add updated server endpoints

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 var express = require('express')
 var app = express()
+var router = express.Router()
+var body_parser = require('body-parser')
 
 app.set('port', (process.env.PORT || 5000))
 
+app.use('/api/v1', router)
+
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+	response.send('Please connect to /api/v1/{endpoint}')
+})
+
+router.get('/', function(request, response) {
+	response.send('Hello World!')
+})
+
+router.get('/animal_name', function(request, response) {
+	var animals = ['cat', 'dog', 'mouse']
+	var adjectives = ['silly', 'big', 'little']	
+	var generated = adjectives[Math.floor(Math.random()*adjectives.length)] + ' ' + animals[Math.floor(Math.random()*animals.length)]
+	response.send(generated)
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+	console.log("Node app is running at localhost:" + app.get('port'))
 })
