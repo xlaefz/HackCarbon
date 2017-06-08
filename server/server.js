d4367acb495ec0cd77e622f629dbd731caf85104
Kaveet Laxmidas <kaveetlaxmidas@gmail.com>
2017-06-07 22:55:25 -0400

    Fix commit

--- server/server.js
+++ server/server.js
@@ -27,11 +27,22 @@ var ref = db.ref("users")
 
 console.log("Getting users")
 
-
+app.use(body_parser.json())
 
 app.set('port', (process.env.PORT || 5000))
 
 app.use('/api/v1', router)
+
+// Convenience function
+
+function pickRandomProperty(obj) {
+    var result;
+    var count = 0;
+    for (var prop in obj)
+        if (Math.random() < 1/++count)
+           result = prop;
+    return result;
+}
 
 ref.on("value", function(snapshot) {
   console.log(snapshot.val());
@@ -59,15 +70,79 @@ router.get('/animal_name', function(request, response) {
 	})
 })
 
-router.get('/validate_chat', function(request, response) {
+/**
+ * Requires a single parameter passed via json: requester
+ * requester is the UUID of the user to be matched
+ * Returns an object:
+ * {
+ * 	"uuid"
+ * 	"name"
+ * }
+ */
+router.post('/find_match', function(request, response) {
+
+	var requester_uuid = request.body.requester
+	var found_uuid
+
+	if (requester_uuid == null) {
+		response.json({
+			"status":"error",
+			"data": null,
+			"message": "No requester UUID was supplied."
+		})
+		return
+	}
+
+	ref.on("value", function(snapshot) {
+		// Pick random from snapshot object
+		var table = snapshot.val()
+		var chosen = null
+
+		do {
+			chosen = pickRandomProperty(table)
+			console.dir('Chosen key', chosen)
+		}
+		while (chosen == requester_uuid)
+
+		if (chosen != null) {
+			response.json({
+				"status":"success",
+				"data": {
+					"uuid":chosen,
+					"name":table[chosen].name
+				},
+				"message": null
+			})
+		}
+		else {
+			response.json({
+				"status":"error",
+				"data": null,
+				"message": "No match was found for the supplied UUID."
+			})
+			return
+		}
+	}, function (errorObject) {
+		response.json({
+			"status":"error",
+			"data": null,
+			"message": "A failure occurred while reading from Firebase."
+		})
+		return
+	})
+
+})
+
+router.post('/validate_chat', function(request, response) {
 	var bad_message = false
 
+	console.log(request.body.text);
+	console.dir(sent(request.body.text))
 	var score = sent(request.body.text).score
 	console.log("The score for this message is " + score)
 	if (score <= -2) {
 		bad_message = true
 	}
-
 	response.json({
 		"status": "success",
 		"data": {
